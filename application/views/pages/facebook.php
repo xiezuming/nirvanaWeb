f623fe6f1da941926d971018f564199975f18aee
xiezuming <xiezuming@gmail.com>
2014-08-16 21:50:17 +0800

    test`

--- application/views/pages/facebook.php
+++ application/views/pages/facebook.php
@@ -1,14 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Facebook</title>
 <script>
 window.onload = function(){
-	document.forms[0].submit();
+	window.location = "https://www.facebook.com/pages/Happitailcom/767469703263125";
 }
 </script>
+</head>
 
-<?php echo form_open('algorithm/query_item_info_by_similar_item')?>
-<?php echo form_hidden('title', $query_title)?>
-<?php echo form_hidden('catNum', $catNum)?>
-<?php echo form_hidden('similarItemUrl', $similarItemUrl)?>
-<?php echo '</form>'?>
+<body>
+
+</body>
+</html>
+
 
 
 
