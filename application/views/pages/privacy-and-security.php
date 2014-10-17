bd4d81f778b1ba4c622c9ec3f842b5652461c909
xiezuming <xiezuming@gmail.com>
2014-10-17 10:12:12 +0800

    Change privacy related content

--- application/views/pages/privacy-and-security.php
+++ application/views/pages/privacy-and-security.php
@@ -1,14 +1,149 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>PRIVACY and SECURITY</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title>Privacy and Security</title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="1265">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 28.0px Verdana}
+    p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; text-align: justify; font: 15.0px Times; min-height: 18.0px}
+    p.p3 {margin: 0.0px 0.0px 0.0px 0.0px; text-align: justify; font: 15.0px Times}
+  </style>
 </head>
 <body>
-<p>Your privacy is important to us. So we've developed a Privacy Policy that covers how we collect, use, disclose, transfer, and store your information.</p> 
-<p>Please take a moment to familiarize yourself with our privacy practices and let us know if you have any questions.</p>
-
-TBD
-
+<p class="p1">www.weee-app.com / Weee! App Privacy Policy</p>
+<p class="p2"><br></p>
+<p class="p2"><br></p>
+<p class="p3">This privacy policy has been compiled to better serve those who are concerned with how their 'Personally identifiable information' (PII) is being used online. PII, as used in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.</p>
+<p class="p2"><br></p>
+<p class="p3"><b>What personal information do we collect from the people that visit our blog, website or app?</b></p>
+<p class="p2"><br></p>
+<p class="p3">When ordering or registering on our site, as appropriate, you may be asked to enter your Name, Email address, Mailing address, Phone number or other details to help you with your experience.</p>
+<p class="p2"><br></p>
+<p class="p3"><b>When do we collect information?</b></p>
+<p class="p2"><br></p>
+<p class="p3">We collect information from you when you register on our site or enter information on our site.</p>
+<p class="p2"><br></p>
+<p class="p2"><br></p>
+<p class="p3"><b>How do we use your information?</b></p>
+<p class="p2"><br></p>
+<p class="p3">We may use the information we collect from you when you register, make a purchase, sign up for our newsletter, respond to a survey or marketing communication, surf the website, or use certain other site features in the following ways:</p>
+<p class="p2"><br></p>
+<p class="p3">      <b>•</b> To personalize user's experience and to allow us to deliver the type of content and product offerings in which you are most interested.</p>
+<p class="p3">      <b>•</b> To improve our website in order to better serve you.</p>
+<p class="p3">      <b>•</b> To allow us to better service you in responding to your customer service requests.</p>
+<p class="p3">      <b>•</b> To administer a contest, promotion, survey or other site feature.</p>
+<p class="p3">      <b>•</b> To quickly process your transactions.</p>
+<p class="p3">      <b>•</b> To send periodic emails regarding your order or other products and services.</p>
+<p class="p2"><br></p>
+<p class="p3"><b>How do we protect visitor information?</b></p>
+<p class="p2"><br></p>
+<p class="p3">We do not use vulnerability scanning and/or scanning to PCI standards.</p>
+<p class="p3">Your personal information is contained behind secured networks and is only accessible by a limited number of persons who have special access rights to such systems, and are required to keep the information confidential. In addition, all sensitive/credit information you supply is encrypted via Secure Socket Layer (SSL) technology.</p>
+<p class="p2"><br></p>
+<p class="p3">We implement a variety of security measures enters, submits, or accesses their information</p>
+<p class="p3">All transactions are processed through a gateway provider and are not stored or processed on our servers.</p>
+<p class="p2"><br></p>
+<p class="p3"><b>Do we use 'cookies'?</b></p>
+<p class="p2"><br></p>
+<p class="p3">We do not use cookies for tracking purposes</p>
+<p class="p2"><br></p>
+<p class="p3">You can choose to have your computer warn you each time a cookie is being sent, or you can choose to turn off all cookies. You do this through your browser (like Internet Explorer) settings. Each browser is a little different, so look at your browser's Help menu to learn the correct way to modify your cookies.</p>
+<p class="p2"><br></p>
+<p class="p3">If you disable cookies off, some features will be disabled that make your site experience more efficient and some of our services will not function properly.</p>
+<p class="p2"><br></p>
+<p class="p3">However, you can still place orders .</p>
+<p class="p2"><br></p>
+<p class="p2"><br></p>
+<p class="p3"><b>Third Party Disclosure</b></p>
+<p class="p2"><br></p>
+<p class="p3">We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information unless we provide you with advance notice. This does not include website hosting partners and other parties who assist us in operating our website, conducting our business, or servicing you, so long as those parties agree to keep this information confidential. We may also release your information when we believe release is appropriate to comply with the law, enforce our site policies, or protect ours or others' rights, property, or safety.<span class="Apple-converted-space"> </span></p>
+<p class="p2"><br></p>
+<p class="p3">However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising, or other uses.</p>
+<p class="p2"><br></p>
+<p class="p3"><b>Third party links</b></p>
+<p class="p2"><br></p>
+<p class="p3">We do not include or offer third party products or services on our website.</p>
+<p class="p2"><br></p>
+<p class="p3"><b>Google</b></p>
+<p class="p2"><br></p>
+<p class="p3">Google's advertising requirements can be summed up by Google's Advertising Principles. They are put in place to provide a positive experience for users. https://support.google.com/adwordspolicy/answer/1316548?hl=en<span class="Apple-converted-space"> </span></p>
+<p class="p2"><br></p>
+<p class="p3">We have not enabled Google AdSense on our site but we may do so in the future.</p>
+<p class="p2"><br></p>
+<p class="p3"><b>California Online Privacy Protection Act</b></p>
+<p class="p2"><br></p>
+<p class="p3">CalOPPA is the first state law in the nation to require commercial websites and online services to post a privacy policy. The law's reach stretches well beyond California to require a person or company in the United States (and conceivably the world) that operates websites collecting personally identifiable information from California consumers to post a conspicuous privacy policy on its website stating exactly the information being collected and those individuals with whom it is being shared, and to comply with this policy. - See more at: http://consumercal.org/california-online-privacy-protection-act-caloppa/#sthash.0FdRbT51.dpuf</p>
+<p class="p2"><br></p>
+<p class="p3"><b>According to CalOPPA we agree to the following:</b></p>
+<p class="p3">Users can visit our site anonymously</p>
+<p class="p3">Once this privacy policy is created, we will add a link to it on our home page, or as a minimum on the first significant page after entering our website.</p>
+<p class="p3">Our Privacy Policy link includes the word 'Privacy', and can be easily be found on the page specified above.</p>
+<p class="p2"><br></p>
+<p class="p3">Users will be notified of any privacy policy changes:</p>
+<p class="p3">      <b>•</b> On our Privacy Policy Page</p>
+<p class="p3">Users are able to change their personal information:</p>
+<p class="p3">      <b>•</b> By emailing us</p>
+<p class="p3">      <b>•</b> By logging in to their account</p>
+<p class="p2"><br></p>
+<p class="p3"><b>How does our site handle do not track signals?</b></p>
+<p class="p3">We honor do not track signals and do not track, plant cookies, or use advertising when a Do Not Track (DNT) browser mechanism is in place.</p>
+<p class="p2"><br></p>
+<p class="p3"><b>Does our site allow third party behavioral tracking?</b></p>
+<p class="p3">It's also important to note that we allow third party behavioral tracking</p>
+<p class="p2"><br></p>
+<p class="p3"><b>COPPA (Children Online Privacy Protection Act)</b></p>
+<p class="p2"><br></p>
+<p class="p3">When it comes to the collection of personal information from children under 13, the Children's Online Privacy Protection Act (COPPA) puts parents in control. The Federal Trade Commission, the nation's consumer protection agency, enforces the COPPA Rule, which spells out what operators of websites and online services must do to protect children's privacy and safety online.</p>
+<p class="p2"><br></p>
+<p class="p3">We do not specifically market to children under 13.</p>
+<p class="p2"><br></p>
+<p class="p3"><b>Fair Information Practices</b></p>
+<p class="p2"><br></p>
+<p class="p3">The Fair Information Practices Principles form the backbone of privacy law in the United States and the concepts they include have played a significant role in the development of data protection laws around the globe. Understanding the Fair Information Practice Principles and how they should be implemented is critical to comply with the various privacy laws that protect personal information.</p>
+<p class="p2"><br></p>
+<p class="p3"><b>In order to be in line with Fair Information Practices we will take the following responsive action, should a data breach occur:</b></p>
+<p class="p3">We will notify the users via email</p>
+<p class="p3">      <b>•</b> Within 7 business days</p>
+<p class="p2"><br></p>
+<p class="p3">We also agree to the individual redress principle, which requires that individuals have a right to pursue legally enforceable rights against data collectors and processors who fail to adhere to the law. This principle requires not only that individuals have enforceable rights against data users, but also that individuals have recourse to courts or a government agency to investigate and/or prosecute non-compliance by data processors.</p>
+<p class="p2"><br></p>
+<p class="p3"><b>CAN SPAM Act</b></p>
+<p class="p2"><br></p>
+<p class="p3">The CAN-SPAM Act is a law that sets the rules for commercial email, establishes requirements for commercial messages, gives recipients the right to have emails stopped from being sent to them, and spells out tough penalties for violations.</p>
+<p class="p2"><br></p>
+<p class="p3"><b>We collect your email address in order to:</b></p>
+<p class="p3">      <b>•</b> Send information, respond to inquiries, and/or other requests or questions.</p>
+<p class="p3">      <b>•</b> Process orders and to send information and updates pertaining to orders</p>
+<p class="p3">      <b>•</b> We may also send you additional information related to your product and/or service.</p>
+<p class="p3">      <b>•</b> Market to our mailing list or continue to send emails to our clients after the original transaction has occurred</p>
+<p class="p2"><br></p>
+<p class="p3"><b>To be accordance with CANSPAM we agree to the following:</b></p>
+<p class="p3">      <b>•</b> NOT use false, or misleading subjects or email addresses</p>
+<p class="p3">      <b>•</b> Identify the message as an advertisement in some reasonable way</p>
+<p class="p3">      <b>•</b> Include the physical address of our business or site headquarters</p>
+<p class="p3">      <b>•</b> Monitor third party email marketing services for compliance, if one is used.</p>
+<p class="p3">      <b>•</b> Honor opt-out/unsubscribe requests quickly</p>
+<p class="p3">      <b>•</b> Allow users to unsubscribe by using the link at the bottom of each email</p>
+<p class="p2"><br></p>
+<p class="p3"><b>If at any time you would like to unsubscribe from receiving future emails, you can</b></p>
+<p class="p3">      <b>•</b> Follow the instructions at the bottom of each email.</p>
+<p class="p2"><br></p>
+<p class="p3"><b>Contacting Us</b></p>
+<p class="p2"><br></p>
+<p class="p3">If there are any questions regarding this privacy policy you may contact us using the information below.</p>
+<p class="p2"><br></p>
+<p class="p3">www.weee-app.com</p>
+<p class="p3">San Jose</p>
+<p class="p3">USA</p>
+<p class="p3">3519 Calvelli Ct</p>
+<p class="p3">CA</p>
+<p class="p3">95124</p>
+<p class="p3">larry@wetagapp.com</p>
+<p class="p3">408-502-3495</p>
+<p class="p2"><br></p>
+<p class="p3">Last Edited on 2014-10-16</p>
 </body>
 </html>
---
+++ application/views/pages/terms-of-service.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title>Terms of Service</title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="1265">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 21.0px Times}
+    p.p2 {margin: 0.0px 0.0px 12.0px 0.0px; font: 12.0px Times}
+    p.p3 {margin: 0.0px 0.0px 0.0px 0.0px; font: 19.0px Times}
+    p.p4 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Times; min-height: 14.0px}
+    p.p5 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Times}
+    span.s1 {font: 19.0px Times}
+    span.s2 {text-decoration: underline ; color: #0000ee}
+    span.s3 {font: 12.0px Times}
+  </style>
+</head>
+<body>
+<p class="p1"><b>Terms of Service for weee-app.com and Weee! App</b></p>
+<p class="p2"><span class="s1">Introduction</span> <br>
+<br>
+Welcome to weee-app.com ("The website") and Weee! App ("The mobile app"). This website and mobile app are owned and operated by weee-app.com. By visiting our website/using our mobile app and accessing the information, resources, services, products, and tools we provide, you understand and agree to accept and adhere to the following terms and conditions as stated in this policy (hereafter referred to as 'User Agreement'), along with the terms and conditions as stated in our Privacy Policy (please refer to the Privacy Policy section below for more information). <br>
+<br>
+This agreement is in effect as of Oct 16, 2014. <br>
+<br>
+We reserve the right to change this User Agreement from time to time without notice. You acknowledge and agree that it is your responsibility to review this User Agreement periodically to familiarize yourself with any modifications. Your continued use of this site after such modifications will constitute acknowledgment and agreement of the modified terms and conditions.<span class="Apple-converted-space"> </span></p>
+<p class="p2"><span class="s1">Responsible Use and Conduct</span> <br>
+<br>
+By visiting our website/mobile app and accessing the information, resources, services, products, and tools we provide for you, either directly or indirectly (hereafter referred to as 'Resources'), you agree to use these Resources only for the purposes intended as permitted by (a) the terms of this User Agreement, and (b) applicable laws, regulations and generally accepted online practices or guidelines. <br>
+<br>
+Wherein, you understand that: <br>
+<br>
+a. In order to access our Resources, you may be required to provide certain information about yourself (such as identification, contact details, etc.) as part of the registration process, or as part of your ability to use the Resources. You agree that any information you provide will always be accurate, correct, and up to date. <br>
+<br>
+b. You are responsible for maintaining the confidentiality of any login information associated with any account you use to access our Resources. Accordingly, you are responsible for all activities that occur under your account/s. <br>
+<br>
+c. Accessing (or attempting to access) any of our Resources by any means other than through the means we provide, is strictly prohibited. You specifically agree not to access (or attempt to access) any of our Resources through any automated, unethical or unconventional means. <br>
+<br>
+d. Engaging in any activity that disrupts or interferes with our Resources, including the servers and/or networks to which our Resources are located or connected, is strictly prohibited. <br>
+<br>
+e. Attempting to copy, duplicate, reproduce, sell, trade, or resell our Resources is strictly prohibited. <br>
+<br>
+f. You are solely responsible any consequences, losses, or damages that we may directly or indirectly incur or suffer due to any unauthorized activities conducted by you, as explained above, and may incur criminal or civil liability. <br>
+<br>
+g. We may provide various open communication tools on our website/mobile app, such as blog comments, blog posts, public chat, forums, message boards, newsgroups, product ratings and reviews, various social media services, etc. You understand that generally we do not pre-screen or monitor the content posted by users of these various communication tools, which means that if you choose to use these tools to submit any type of content to our website/mobile app, then it is your personal responsibility to use these tools in a responsible and ethical manner. By posting information or otherwise using any open communication tools as mentioned, you agree that you will not upload, post, share, or otherwise distribute any content that: <br>
+<br>
+i. Is illegal, threatening, defamatory, abusive, harassing, degrading, intimidating, fraudulent, deceptive, invasive, racist, or contains any type of suggestive, inappropriate, or explicit language;<br>
+ii. Infringes on any trademark, patent, trade secret, copyright, or other proprietary right of any party;<br>
+Iii. Contains any type of unauthorized or unsolicited advertising;<br>
+Iiii. Impersonates any person or entity, including any weee-app.com employees or representatives.<br>
+<br>
+<br>
+We have the right at our sole discretion to remove any content that, we feel in our judgment does not comply with this User Agreement, along with any content that we feel is otherwise offensive, harmful, objectionable, inaccurate, or violates any 3rd party copyrights or trademarks. We are not responsible for any delay or failure in removing such content. If you post content that we choose to remove, you hereby consent to such removal, and consent to waive any claim against us. <br>
+<br>
+h. We do not assume any liability for any content posted by you or any other 3rd party users of our website/mobile app. However, any content posted by you using any open communication tools on our website/mobile app, provided that it doesn't violate or infringe on any 3rd party copyrights or trademarks, becomes the property of weee-app.com, and as such, gives us a perpetual, irrevocable, worldwide, royalty-free, exclusive license to reproduce, modify, adapt, translate, publish, publicly display and/or distribute as we see fit. This only refers and applies to content posted via open communication tools as described, and does not refer to information that is provided as part of the registration process, necessary in order to use our Resources. All information provided as part of our registration process is covered by our <a href="http://www.wetagapp.com/privacy"><span class="s2">privacy policy</span></a>. <br>
+<br>
+i. You agree to indemnify and hold harmless weee-app.com and its parent company and affiliates, and their directors, officers, managers, employees, donors, agents, and licensors, from and against all losses, expenses, damages and costs, including reasonable attorneys' fees, resulting from any violation of this User Agreement or the failure to fulfill any obligations relating to your account incurred by you or any other person using your account. We reserve the right to take over the exclusive defense of any claim for which we are entitled to indemnification under this User Agreement. In such event, you shall provide us with such cooperation as is reasonably requested by us.<span class="Apple-converted-space"> </span></p>
+<p class="p3">Privacy<span class="s3"><span class="Apple-converted-space"> </span></span></p>
+<p class="p4"><br></p>
+<p class="p5">Your privacy is very important to us, which is why we've created a separate Privacy Policy in order to explain in detail how we collect, manage, process, secure, and store your private information. Our privacy policy is included under the scope of this User Agreement. To read our privacy policy in its entirety, <a href="http://www.wetagapp.com/privacy"><span class="s2">click here</span></a>.<span class="Apple-converted-space"> </span></p>
+<p class="p4"><br></p>
+<p class="p2"><span class="s1">Limitation of Warranties</span> <br>
+<br>
+By using our website/mobile app, you understand and agree that all Resources we provide are "as is" and "as available". This means that we do not represent or warrant to you that:<br>
+i) the use of our Resources will meet your needs or requirements.<br>
+ii) the use of our Resources will be uninterrupted, timely, secure or free from errors.<br>
+iii) the information obtained by using our Resources will be accurate or reliable, and<br>
+iv) any defects in the operation or functionality of any Resources we provide will be repaired or corrected.<br>
+<br>
+<br>
+Furthermore, you understand and agree that: <br>
+<br>
+v) any content downloaded or otherwise obtained through the use of our Resources is done at your own discretion and risk, and that you are solely responsible for any damage to your computer or other devices for any loss of data that may result from the download of such content.<br>
+vi) no information or advice, whether expressed, implied, oral or written, obtained by you from weee-app.com or through any Resources we provide shall create any warranty, guarantee, or conditions of any kind, except for those expressly outlined in this User Agreement.<br>
+</p>
+<p class="p3">Limitation of Liability<span class="s3"><span class="Apple-converted-space"> </span></span></p>
+<p class="p4"><br></p>
+<p class="p5">In conjunction with the Limitation of Warranties as explained above, you expressly understand and agree that any claim against us shall be limited to the amount you paid, if any, for use of products and/or services. weee-app.com will not be liable for any direct, indirect, incidental, consequential or exemplary loss or damages which may be incurred by you as a result of using our Resources, or as a result of any changes, data loss or corruption, cancellation, loss of access, or downtime to the full extent that applicable limitation of liability laws apply.<span class="Apple-converted-space"> </span></p>
+<p class="p4"><br></p>
+<p class="p2"><span class="s1">Copyrights/Trademarks</span> <br>
+<br>
+All content and materials available on weee-app.com/WeTag app, including but not limited to text, graphics, website name, code, images and logos are the intellectual property of weee-app.com, and are protected by applicable copyright and trademark law. Any inappropriate use, including but not limited to the reproduction, distribution, display or transmission of any content on this site is strictly prohibited, unless specifically authorized by weee-app.com.<span class="Apple-converted-space"> </span></p>
+<p class="p2"><span class="s1">Termination of Use</span> <br>
+<br>
+You agree that we may, at our sole discretion, suspend or terminate your access to all or part of our website/mobile app and Resources with or without notice and for any reason, including, without limitation, breach of this User Agreement. Any suspected illegal, fraudulent or abusive activity may be grounds for terminating your relationship and may be referred to appropriate law enforcement authorities. Upon suspension or termination, your right to use the Resources we provide will immediately cease, and we reserve the right to remove or delete any information that you may have on file with us, including any account or login information.<span class="Apple-converted-space"> </span></p>
+<p class="p2"><span class="s1">Governing Law</span> <br>
+<br>
+This website is controlled by weee-app.com. It can be accessed by most countries around the world. By accessing our website, you agree that the statutes and laws of our state, without regard to the conflict of laws and the United Nations Convention on the International Sales of Goods, will apply to all matters relating to the use of this website and the purchase of any products or services through this site. <br>
+<br>
+Furthermore, any action to enforce this User Agreement shall be brought in the federal or state courts You hereby agree to personal jurisdiction by such courts, and waive any jurisdictional, venue, or inconvenient forum objections to such courts.<span class="Apple-converted-space"> </span></p>
+<p class="p3">Guarantee<span class="s3"><span class="Apple-converted-space"> </span></span></p>
+<p class="p4"><br></p>
+<p class="p5">UNLESS OTHERWISE EXPRESSED, weee-app.com EXPRESSLY DISCLAIMS ALL WARRANTIES AND CONDITIONS OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES AND CONDITIONS OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.<span class="Apple-converted-space"> </span></p>
+<p class="p4"><br></p>
+<p class="p3">Contact Information<span class="s3"><span class="Apple-converted-space"> </span></span></p>
+<p class="p4"><br></p>
+<p class="p5">If you have any questions or comments about these our Terms of Service as outlined above, you can contact us at:</p>
+<p class="p4"><br></p>
+<p class="p5">weee-app.com</p>
+<p class="p4"><br></p>
+<p class="p4"><br></p>
+<p class="p4"><br></p>
+<p class="p4"><br></p>
+</body>
+</html>
