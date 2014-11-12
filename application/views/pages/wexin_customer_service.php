d09949fc9be1689c374d56226eb2004e488f6c5f
xiezuming <xiezuming@gmail.com>
2014-11-12 23:46:16 +0800

    adaptor for mobile

--- application/views/pages/wexin_customer_service.php
+++ application/views/pages/wexin_customer_service.php
@@ -1,8 +1,12 @@
 <html>
 <head>
-<title>Help</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"
+	lang="en-us">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Customer Service</title>
 </head>
 <body>
-	<p>TBD</p>
+	<p>Email: larry@differentsocial.com</p>
+	<p>WeChat ID: larry_weee</p>
 </body>
 </html>
--- application/views/pages/wexin_help.php
+++ application/views/pages/wexin_help.php
@@ -1,9 +1,11 @@
 <html>
 <head>
-<title>Customer Service</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"
+	lang="en-us">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Help</title>
 </head>
 <body>
-	<p>Email: larry@differentsocial.com</p>
-	<p>WeChat ID: larry_weee</p>
+	<p>TBD</p>
 </body>
 </html>
